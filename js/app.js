5c50b31d2b1a123b40469b4d05eab84023b6909b
zayanahmed925 <rjraju925@gmail.com>
2022-02-17 19:36:23 +0600

    compleate without error handeling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 //Get input value
 function getInputValue(inputId) {
     const getInput = document.getElementById(inputId);
@@ -5,8 +6,25 @@ function getInputValue(inputId) {
     const getInputAmount = parseFloat(getInputText);
     return getInputAmount;
 }
+// Total Cost
+function totalExpenses() {
+    const foodCost = getInputValue('food-cost');
+    const rentCost = getInputValue('rent-cost');
+    const clothesCost = getInputValue('clothes-cost');
+    const totalCost = foodCost + rentCost + clothesCost
+    return totalCost;
+}
+// function getTotalIncome() {
+//     const totalIncome = getInputValue('total-income');
+//     return totalIncome;
+// }
+// function getBalance() {
+//     const balance = getTotalIncome - totalExpenses();
+//     return balance;
+// }
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
+    console.log("hi");
     //Total Income
     /* const totalIncome = document.getElementById('total-income')
     const totalIncomeText = totalIncome.value;
@@ -19,40 +37,72 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const foodCostText = foodCost.value;
     const foodCostAmount = parseFloat(foodCostText);
     console.log(foodCostAmount); */
-    const foodCost = getInputValue('food-cost');
-    //Rent cost
-    /* const rentCost = document.getElementById('rent-cost');
-    const rentCostText = rentCost.value;
-    const rentCostAmount = parseFloat(rentCostText);
-    console.log(rentCostAmount); */
-    const rentCost = getInputValue('rent-cost');
-    // Clothes cost
-    /* const clothesCost = document.getElementById('clothes-cost');
-    const clothesCostText = clothesCost.value;
-    const clothesCostAmount = parseFloat(clothesCostText);
-    console.log(clothesCostAmount); */
-    const clothesCost = getInputValue('clothes-cost');
+    // const foodCost = getInputValue('food-cost');
+    // //Rent cost
+    // /* const rentCost = document.getElementById('rent-cost');
+    // const rentCostText = rentCost.value;
+    // const rentCostAmount = parseFloat(rentCostText);
+    // console.log(rentCostAmount); */
+    // const rentCost = getInputValue('rent-cost');
+    // // Clothes cost
+    // /* const clothesCost = document.getElementById('clothes-cost');
+    // const clothesCostText = clothesCost.value;
+    // const clothesCostAmount = parseFloat(clothesCostText);
+    // console.log(clothesCostAmount); */
+    // const clothesCost = getInputValue('clothes-cost');
 
-    // total Cost add
-    const totalCost = foodCost + rentCost + clothesCost
+    // // total Cost add
+    // const totalCost = foodCost + rentCost + clothesCost
     // console.log(totalCost);
 
     // Total Expenses
     const TotalExpensesInput = document.getElementById('total-expenses')
-    const totalExpensesText = TotalExpensesInput.innerText;
-    const totalExpensesPreviousAmount = parseFloat(totalExpensesText);
-    const totalExpensesUpdateAmount = totalExpensesPreviousAmount + totalCost;
-    TotalExpensesInput.innerText = totalExpensesUpdateAmount;
+    // const totalExpensesText = TotalExpensesInput.innerText;
+    // const totalExpensesPreviousAmount = parseFloat(totalExpensesText);
+    // const totalExpensesUpdateAmount = totalExpensesPreviousAmount + totalCost;
+    TotalExpensesInput.innerText = totalExpenses();
 
     //balance Calculation
-    const Balance = totalIncome - totalCost;
+    const Balance = totalIncome - totalExpenses();
 
     //balance
     const totalBalance = document.getElementById('balance')
-    const totalBalanceText = totalBalance.innerText;
-    const totalPreviousBalance = parseFloat(totalBalanceText);
-    const totalUpdateBalance = totalPreviousBalance + Balance;
-    totalBalance.innerText = totalUpdateBalance;
+    // const totalBalanceText = totalBalance.innerText;
+    // const totalPreviousBalance = parseFloat(totalBalanceText);
+    // const totalUpdateBalance = totalPreviousBalance + Balance;
+    totalBalance.innerText = Balance;
 
+
+})
+document.getElementById('saving-btn').addEventListener('click', function () {
+
+    // pore dlt kore dibo
+    // const totalIncome1 = getInputValue('total-income');
+    const totalIncome1 = getInputValue('total-income');
+    /////////////////////////////////////////
+
+
+    // Saving Percentage
+    const getSavingPercentage = getInputValue('saving-Percentage')
+    const getSavingAmount = (totalIncome1 * getSavingPercentage) / 100;
+    // console.log(getSavingAmount);
+    //saving amount display
+    const savingAmount = document.getElementById('saving-amount');
+    // const savingAmountText = savingAmount.innerText;
+    // const savingPreviousAmount = parseFloat(savingAmountText);
+    // const totalUpdateBalance = savingPreviousAmount + savingCalculation;
+    savingAmount.innerText = getSavingAmount;
+
+    // Remaning Balance 
+    // [aikhaner last part ta akto pblm ace uporer function er value needed]
+
+    // pore dlt kore dibo
+    const totalIncome = getInputValue('total-income');
+    const Balance1 = totalIncome - totalExpenses();
+    /////////////////////////////////////////
+
+    const remaningBalance = document.getElementById('remaning-balance');
+    const getRemaningBalance = Balance1 - getSavingAmount;
+    remaningBalance.innerText = getRemaningBalance;
 
 })
